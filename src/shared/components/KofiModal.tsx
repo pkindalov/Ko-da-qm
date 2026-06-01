3d083da32f9d4cad293a8607e03dafb3dca0cae2
pkindalov <panayotkindalov@gmail.com>
2026-06-01 09:38:35 +0300

    Fix: Ko-fi widget loads blank on mobile due to preview=true flag

--- src/shared/components/KofiModal.tsx
+++ src/shared/components/KofiModal.tsx
@@ -10,7 +10,7 @@ interface KofiModalProps {
 export const KofiModal = ({ open, onClose, lang }: KofiModalProps) => (
   <Modal open={open} onClose={onClose} contentClassName="modal-kofi">
     <iframe
-      src="https://ko-fi.com/pkindalov/?hidefeed=true&widget=true&embed=true&preview=true"
+      src="https://ko-fi.com/pkindalov/?hidefeed=true&widget=true&embed=true"
       title={lang === 'en' ? 'Support this project on Ko-fi' : 'Подкрепи проекта в Ko-fi'}
       className="kofi-iframe"
       allow="payment"
